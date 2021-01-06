f2760b285701a8412ecb390f5aa74d81408403ef
Owen Moore <hello@owenmoore.xyz>
2021-01-05 20:52:52 -0800

    almost done refactor

--- CubeCoordinates/Runtime/Coordinates.cs
+++ CubeCoordinates/Runtime/Coordinates.cs
@@ -139,30 +139,6 @@ namespace CubeCoordinates
             foreach (Vector3 cube in cubes)
             {
                 if (container.GetCoordinate(cube) != null) continue;
-
-                /*
-                GameObject go = null;
-                string label = "Coordinate: [" + cube.x + "," + cube.y + "," + cube.z + "]";
-
-                switch (_coordinateDisplayType)
-                {
-                    case Coordinate.DisplayType.None:
-                        go = new GameObject(label);
-                        break;
-
-                    case Coordinate.DisplayType.GenerateMesh:
-                        go = (GameObject)Instantiate(_coordinateGameObject, transform.position, Quaternion.identity);
-                        go.name = label;
-                        go.SetActive(true);
-                        break;
-                }
-                go.transform.parent = _coordinateGroup.transform;
-                */
-                //Coordinate coordinate = go.AddComponent<Coordinate>();
-                //coordinate.Init(
-                //    cube,
-                //    ConvertCubeToWorldPosition(cube)
-                //);
                 Coordinate coordinate =
                     new Coordinate(cube, ConvertCubeToWorldPosition(cube));
                 coordinates.Add (coordinate);
@@ -220,120 +196,23 @@ namespace CubeCoordinates
                 _containers.Add(key, new Container(key));
         }
 
-        private Vector2 ConvertCubeToAxial(Vector3 cube)
+        public Coordinate
+        GetNeighbor(
+            Coordinate origin,
+            int direction,
+            int distance,
+            string container_label = "all"
+        )
         {
-            return new Vector2(cube.x, cube.z);
-        }
-
-        private Vector3 ConvertAxialToCube(Vector2 axial)
-        {
-            return new Vector3(axial.x, (-axial.x - axial.y), axial.y);
-        }
-
-        private Vector3 ConvertAxialToWorldPosition(Vector2 axial)
-        {
-            return new Vector3(axial.x * _spacingX,
-                0.0f,
-                -((axial.x * _spacingZ) + (axial.y * _spacingZ * 2.0f)));
-        }
-
-        private Vector3 ConvertCubeToWorldPosition(Vector3 cube)
-        {
-            return new Vector3(cube.x * _spacingX,
-                0.0f,
-                -((cube.x * _spacingZ) + (cube.z * _spacingZ * 2.0f)));
-        }
-
-        private Vector2 ConvertWorldPositionToAxial(Vector3 wPos)
-        {
-            float q = (wPos.x * (2.0f / 3.0f)) / _radius;
-            float r =
-                ((-wPos.x / 3.0f) + ((Mathf.Sqrt(3) / 3.0f) * wPos.z)) /
-                _radius;
-            return RoundAxial(new Vector2(q, r));
-        }
-
-        private Vector3 ConvertWorldPositionToCube(Vector3 wPos)
-        {
-            return ConvertAxialToCube(ConvertWorldPositionToAxial(wPos));
-        }
-
-        private Vector2 RoundAxial(Vector2 axial)
-        {
-            return RoundCube(ConvertAxialToCube(axial));
-        }
-
-        private Vector3 RoundCube(Vector3 cube)
-        {
-            float rx = Mathf.Round(cube.x);
-            float ry = Mathf.Round(cube.y);
-            float rz = Mathf.Round(cube.z);
-
-            float x_diff = Mathf.Abs(rx - cube.x);
-            float y_diff = Mathf.Abs(ry - cube.y);
-            float z_diff = Mathf.Abs(rz - cube.z);
-
-            if (x_diff > y_diff && x_diff > z_diff)
-                rx = -ry - rz;
-            else if (y_diff > z_diff)
-                ry = -rx - rz;
-            else
-                rz = -rx - ry;
-
-            return new Vector3(rx, ry, rz);
-        }
-
-        public Vector3 GetNeighborCube(Vector3 cube, int direction)
-        {
-            return GetNeighborCube(cube, direction, 1);
-        }
-
-        public Vector3
-        GetNeighborCube(Vector3 cube, int direction, int distance)
-        {
-            return cube + (_directions[direction] * (float) distance);
-        }
-
-        public List<Vector3> GetNeighborCubes(Vector3 cube, bool raw = false)
-        {
-            return GetNeighborCubes(cube, 1, raw);
-        }
-
-        public List<Vector3>
-        GetNeighborCubes(Vector3 cube, int radius, bool raw = false)
-        {
-            List<Vector3> cubes = new List<Vector3>();
-            for (
-                int x = (int)(cube.x - radius);
-                x <= (int)(cube.x + radius);
-                x++
-            )
-            {
-                for (
-                    int y = (int)(cube.y - radius);
-                    y <= (int)(cube.y + radius);
-                    y++
-                )
-                {
-                    for (
-                        int z = (int)(cube.z - radius);
-                        z <= (int)(cube.z + radius);
-                        z++
-                    )
-                    {
-                        if ((x + y + z) == 0) cubes.Add(new Vector3(x, y, z));
-                    }
-                }
-            }
-
-            cubes.Remove (cube);
-            return raw ? cubes : CleanCubeResults(cubes);
+            return GetContainer(container_label)
+                .GetCoordinate(origin.cube +
+                _directions[direction] * (float) distance);
         }
 
         public List<Coordinate>
         GetNeighbors(
             Coordinate origin,
-            int radius,
+            int steps,
             string container_label = "all"
         )
         {
@@ -344,27 +223,27 @@ namespace CubeCoordinates
             List<Vector3> cubes = container.GetAllCubes();
 
             for (
-                int x = (int)(origin.cube.x - radius);
-                x <= (int)(origin.cube.x + radius);
+                int x = (int)(origin.cube.x - steps);
+                x <= (int)(origin.cube.x + steps);
                 x++
             )
             {
                 for (
-                    int y = (int)(origin.cube.y - radius);
-                    y <= (int)(origin.cube.y + radius);
+                    int y = (int)(origin.cube.y - steps);
+                    y <= (int)(origin.cube.y + steps);
                     y++
                 )
                 {
                     for (
-                        int z = (int)(origin.cube.z - radius);
-                        z <= (int)(origin.cube.z + radius);
+                        int z = (int)(origin.cube.z - steps);
+                        z <= (int)(origin.cube.z + steps);
                         z++
                     )
                     {
                         if ((x + y + z) == 0)
                         {
                             Vector3 cube = new Vector3(x, y, z);
-                            if (cubes.Contains(cube))
+                            if (cubes.Contains(cube) && cube != origin.cube)
                             {
                                 results
                                     .Add(coordinates.Find(e => e.cube == cube));
@@ -376,40 +255,45 @@ namespace CubeCoordinates
             return results;
         }
 
-        public Vector3 GetDiagonalNeighborCube(Vector3 cube, int direction)
+        public Coordinate
+        GetDiagonalNeighbor(
+            Coordinate origin,
+            int direction,
+            int distance,
+            string container_label = "all"
+        )
         {
-            return cube + _diagonals[direction];
+            return GetContainer(container_label)
+                .GetCoordinate(origin.cube +
+                _diagonals[direction] * (float) distance);
         }
 
-        public Vector3
-        GetDiagonalNeighborCube(Vector3 cube, int direction, int distance)
+        public List<Coordinate>
+        GetDiagonalNeighbors(
+            Coordinate origin,
+            int distance,
+            string container_label = "all"
+        )
         {
-            return cube + _diagonals[direction] * (float) distance;
-        }
-
-        public List<Vector3>
-        GetDiagonalNeighborCubes(Vector3 cube, bool raw = false)
-        {
-            List<Vector3> cubes = new List<Vector3>();
-            cubes = GetDiagonalNeighborCubes(cube, 1);
-            return raw ? cubes : CleanCubeResults(cubes);
-        }
-
-        public List<Vector3>
-        GetDiagonalNeighborCubes(Vector3 cube, int distance, bool raw = false)
-        {
-            List<Vector3> cubes = new List<Vector3>();
+            List<Coordinate> results = new List<Coordinate>();
+            Container container = GetContainer(container_label);
             for (int i = 0; i < 6; i++)
-            cubes.Add(GetDiagonalNeighborCube(cube, i, distance));
-            return raw ? cubes : CleanCubeResults(cubes);
+            {
+                Coordinate coordinate =
+                    GetDiagonalNeighbor(origin, i, distance, container_label);
+                if (coordinate != null) results.Add(coordinate);
+            }
+            return results;
         }
 
-        public List<Vector3>
-        BooleanCombineCubes(List<Vector3> a, List<Vector3> b, bool raw = false)
+        public List<Coordinate>
+        BooleanCombine(List<Coordinate> a, List<Coordinate> b)
         {
-            List<Vector3> cubes = a;
-            foreach (Vector3 vb in b) if (!a.Contains(vb)) a.Add(vb);
-            return raw ? cubes : CleanCubeResults(cubes);
+            List<Coordinate> results = new List<Coordinate>();
+            results.AddRange (a);
+            foreach (Coordinate c in b)
+            if (!results.Contains(c)) results.Add(c);
+            return results;
         }
 
         public List<Coordinate>
@@ -429,64 +313,28 @@ namespace CubeCoordinates
             return results;
         }
 
-        public List<Vector3>
-        BooleanIntersectionCubes(
-            List<Vector3> a,
-            List<Vector3> b,
-            bool raw = false
-        )
+        public List<Coordinate>
+        BooleanIntersect(List<Coordinate> a, List<Coordinate> b)
         {
-            List<Vector3> cubes = new List<Vector3>();
-            foreach (Vector3 va in a)
-            foreach (Vector3 vb in b) if (va == vb) cubes.Add(va);
-            return raw ? cubes : CleanCubeResults(cubes);
-        }
-
-        public List<Vector3>
-        BooleanExclusionCubes(
-            List<Vector3> a,
-            List<Vector3> b,
-            bool raw = false
-        )
-        {
-            List<Vector3> cubes = new List<Vector3>();
-            foreach (Vector3 va in a)
-            foreach (Vector3 vb in b)
-            if (va != vb)
+            List<Coordinate> results = new List<Coordinate>();
+            foreach (Coordinate ca in a)
             {
-                cubes.Add (va);
-                cubes.Add (vb);
+                foreach (Coordinate cb in b)
+                {
+                    if (ca == cb) results.Add(ca);
+                }
             }
-            return raw ? cubes : CleanCubeResults(cubes);
+            return results;
         }
 
-        public Vector3 RotateCubeCoordinatesRight(Vector3 cube)
+        public List<Coordinate>
+        BooleanExclude(List<Coordinate> a, List<Coordinate> b)
         {
-            return new Vector3(-cube.z, -cube.x, -cube.y);
+            return BooleanDifference(BooleanCombine(a, b),
+            BooleanIntersect(a, b));
         }
 
-        public Vector3 RotateCubeCoordinatesLeft(Vector3 cube)
-        {
-            return new Vector3(-cube.y, -cube.z, -cube.x);
-        }
-
-        public float GetDistanceBetweenTwoCubes(Vector3 a, Vector3 b)
-        {
-            return Mathf
-                .Max(Mathf.Abs(a.x - b.x),
-                Mathf.Abs(a.y - b.y),
-                Mathf.Abs(a.z - b.z));
-        }
-
-        public Vector3 GetLerpBetweenTwoCubes(Vector3 a, Vector3 b, float t)
-        {
-            Vector3 cube =
-                new Vector3(a.x + (b.x - a.x) * t,
-                    a.y + (b.y - a.y) * t,
-                    a.z + (b.z - a.z) * t);
-
-            return cube;
-        }
+        /*
 
         public Vector3
         GetPointOnLineBetweenTwoCubes(Vector3 a, Vector3 b, int distance)
@@ -511,41 +359,7 @@ namespace CubeCoordinates
             return raw ? cubes : CleanCubeResults(cubes);
         }
 
-        public List<Vector3> GetReachableCubes(Vector3 cube)
-        {
-            return GetReachableCubes(cube, 1);
-        }
-
-        public List<Vector3> GetReachableCubes(Vector3 cube, int radius)
-        {
-            if (radius == 1) return GetReachableCubes(cube);
-
-            Container container = GetContainer();
-            List<Vector3> visited = new List<Vector3>();
-            visited.Add (cube);
-
-            List<List<Vector3>> fringes = new List<List<Vector3>>();
-            fringes.Add(new List<Vector3>());
-            fringes[0].Add(cube);
-
-            for (int i = 1; i <= radius; i++)
-            {
-                fringes.Add(new List<Vector3>());
-                foreach (Vector3 v in fringes[i - 1])
-                {
-                    foreach (Vector3 n in GetNeighborCubes(v))
-                    {
-                        if (!visited.Contains(n))
-                        {
-                            visited.Add (n);
-                            fringes[i].Add(n);
-                        }
-                    }
-                }
-            }
-            return visited;
-        }
-
+        */
         public List<Coordinate>
         GetExpand(Coordinate origin, int steps, string container_label = "all")
         {
@@ -588,32 +402,38 @@ namespace CubeCoordinates
             return results;
         }
 
-        public List<Vector3>
-        GetSpiralCubes(Vector3 cube, int radius, bool raw = false)
+        public List<Coordinate>
+        GetRing(Coordinate origin, int distance, string container_label = "all")
         {
-            List<Vector3> cubes = new List<Vector3>();
-            Vector3 current = cube + _directions[4] * (float) radius;
+            List<Coordinate> results = new List<Coordinate>();
+            Container container = GetContainer(container_label);
 
+            Vector3 current = origin.cube + _directions[4] * (float) distance;
             for (int i = 0; i < 6; i++)
-            for (int j = 0; j < radius; j++)
             {
-                cubes.Add (current);
-                current = GetNeighborCube(current, i);
+                for (int j = 0; j < distance; j++)
+                {
+                    Coordinate coordinate = container.GetCoordinate(current);
+                    if (coordinate != null) results.Add(coordinate);
+                    current = current + _directions[i];
+                }
             }
 
-            return raw ? cubes : CleanCubeResults(cubes);
+            return results;
         }
 
-        public List<Vector3>
-        GetMultiSpiralCubes(Vector3 cube, int radius, bool raw = false)
+        public List<Coordinate>
+        GetSpiral(Coordinate origin, int steps, string container_label = "all")
         {
-            List<Vector3> cubes = new List<Vector3>();
-            cubes.Add (cube);
-            for (int i = 0; i <= radius; i++)
-            foreach (Vector4 r in GetSpiralCubes(cube, i)) cubes.Add(r);
-            return raw ? cubes : CleanCubeResults(cubes);
+            List<Coordinate> results = new List<Coordinate>();
+            Container container = GetContainer(container_label);
+            results.Add (origin);
+            for (int i = 0; i <= steps; i++)
+            results.AddRange(GetRing(origin, i));
+            return results;
         }
 
+        /*
         public List<Vector3>
         GetPathBetweenTwoCubes(
             Vector3 origin,
@@ -707,7 +527,8 @@ namespace CubeCoordinates
 
             return path;
         }
-
+        */
+        /*
         public List<Vector3> CleanCubeResults(List<Vector3> cubes)
         {
             Container container = GetContainer();
@@ -716,5 +537,99 @@ namespace CubeCoordinates
             if (container.GetCoordinate(cube) != null) r.Add(cube);
             return r;
         }
+        */
+
+#region private
+        private Vector3 RotateCubeCoordinatesRight(Vector3 cube)
+        {
+            return new Vector3(-cube.z, -cube.x, -cube.y);
+        }
+
+        private Vector3 RotateCubeCoordinatesLeft(Vector3 cube)
+        {
+            return new Vector3(-cube.y, -cube.z, -cube.x);
+        }
+
+        private Vector2 ConvertCubeToAxial(Vector3 cube)
+        {
+            return new Vector2(cube.x, cube.z);
+        }
+
+        private Vector3 ConvertAxialToCube(Vector2 axial)
+        {
+            return new Vector3(axial.x, (-axial.x - axial.y), axial.y);
+        }
+
+        private Vector3 ConvertAxialToWorldPosition(Vector2 axial)
+        {
+            return new Vector3(axial.x * _spacingX,
+                0.0f,
+                -((axial.x * _spacingZ) + (axial.y * _spacingZ * 2.0f)));
+        }
+
+        private Vector3 ConvertCubeToWorldPosition(Vector3 cube)
+        {
+            return new Vector3(cube.x * _spacingX,
+                0.0f,
+                -((cube.x * _spacingZ) + (cube.z * _spacingZ * 2.0f)));
+        }
+
+        private Vector2 ConvertWorldPositionToAxial(Vector3 wPos)
+        {
+            float q = (wPos.x * (2.0f / 3.0f)) / _radius;
+            float r =
+                ((-wPos.x / 3.0f) + ((Mathf.Sqrt(3) / 3.0f) * wPos.z)) /
+                _radius;
+            return RoundAxial(new Vector2(q, r));
+        }
+
+        private Vector3 ConvertWorldPositionToCube(Vector3 wPos)
+        {
+            return ConvertAxialToCube(ConvertWorldPositionToAxial(wPos));
+        }
+
+        private Vector2 RoundAxial(Vector2 axial)
+        {
+            return RoundCube(ConvertAxialToCube(axial));
+        }
+
+        private Vector3 RoundCube(Vector3 cube)
+        {
+            float rx = Mathf.Round(cube.x);
+            float ry = Mathf.Round(cube.y);
+            float rz = Mathf.Round(cube.z);
+
+            float x_diff = Mathf.Abs(rx - cube.x);
+            float y_diff = Mathf.Abs(ry - cube.y);
+            float z_diff = Mathf.Abs(rz - cube.z);
+
+            if (x_diff > y_diff && x_diff > z_diff)
+                rx = -ry - rz;
+            else if (y_diff > z_diff)
+                ry = -rx - rz;
+            else
+                rz = -rx - ry;
+
+            return new Vector3(rx, ry, rz);
+        }
+
+        private float GetDistanceBetweenTwoCubes(Vector3 a, Vector3 b)
+        {
+            return Mathf
+                .Max(Mathf.Abs(a.x - b.x),
+                Mathf.Abs(a.y - b.y),
+                Mathf.Abs(a.z - b.z));
+        }
+
+        private Vector3 GetLerpBetweenTwoCubes(Vector3 a, Vector3 b, float t)
+        {
+            Vector3 cube =
+                new Vector3(a.x + (b.x - a.x) * t,
+                    a.y + (b.y - a.y) * t,
+                    a.z + (b.z - a.z) * t);
+
+            return cube;
+        }
+#endregion
     }
 }
